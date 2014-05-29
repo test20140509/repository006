f1d45d7a3d265926ae71c1037a559685d74ed26b
nishimura.takeshi <nishimura.takeshi@zeevaa.com>
2014-05-29 12:39:46 +0900

    DebugKitの導入

--- php_project/app/Config/core.php
+++ php_project/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhbWwvG93b0qyJfIxfs2gaC9miguVoUuniR2G0F');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '76428593096576836454535496749');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
